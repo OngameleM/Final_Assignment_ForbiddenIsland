a20f783c02d8873bf14cc4dc0057e45167657f8d
redmartian-droid <118920978+redmartian-droid@users.noreply.github.com>
2023-06-21 12:08:37 +0200

    Updated grid movements

--- Final_Assignment_ForbiddenIsland/Forbidden Island/Assets/Scripts/Scripts/GridMovement.cs
+++ Final_Assignment_ForbiddenIsland/Forbidden Island/Assets/Scripts/Scripts/GridMovement.cs
@@ -4,34 +4,39 @@ using UnityEngine;
 
 public class GridMovement : MonoBehaviour
 {
+    public float gridSizeX = 1f; 
+    public float gridSizeY = 1f; 
+    public int maxSteps = 3; 
+
     private bool isMoving;
     private Vector3 origPos, targetPos;
     private float timeToMove = 0.2f;
+    private int stepsTaken = 0;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W) && !isMoving)
+        if (Input.GetKey(KeyCode.W) && !isMoving && stepsTaken < maxSteps)
             StartCoroutine(MovePlayer(Vector3.up));
 
-        if (Input.GetKey(KeyCode.A) && !isMoving)
+        if (Input.GetKey(KeyCode.A) && !isMoving && stepsTaken < maxSteps)
             StartCoroutine(MovePlayer(Vector3.left));
 
-        if (Input.GetKey(KeyCode.S) && !isMoving)
+        if (Input.GetKey(KeyCode.S) && !isMoving && stepsTaken < maxSteps)
             StartCoroutine(MovePlayer(Vector3.down));
 
-        if (Input.GetKey(KeyCode.D) && !isMoving)
+        if (Input.GetKey(KeyCode.D) && !isMoving && stepsTaken < maxSteps)
             StartCoroutine(MovePlayer(Vector3.right));
     }
 
-    private IEnumerator MovePlayer (Vector3 direction)
+    private IEnumerator MovePlayer(Vector3 direction)
     {
         isMoving = true;
 
         float elapsedTime = 0;
 
         origPos = transform.position;
-        targetPos = origPos + direction;
+        targetPos = origPos + new Vector3(direction.x * gridSizeX, direction.y * gridSizeY, 0f);
 
         while (elapsedTime < timeToMove)
         {
