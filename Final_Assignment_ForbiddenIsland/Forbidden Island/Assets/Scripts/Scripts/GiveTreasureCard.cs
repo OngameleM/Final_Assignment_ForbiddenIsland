e26c6de6ffb54ccef9d33be324fc320c93ee5de4
redmartian-droid <118920978+redmartian-droid@users.noreply.github.com>
2023-06-25 19:45:30 +0200

    Additional cards

--- Final_Assignment_ForbiddenIsland/Forbidden Island/Assets/Scripts/Scripts/GiveTreasureCard.cs
+++ Final_Assignment_ForbiddenIsland/Forbidden Island/Assets/Scripts/Scripts/GiveTreasureCard.cs
@@ -10,23 +10,32 @@ public class GiveTreasureCard : MonoBehaviour
 
     public void TransferSprites()
     {
-        Debug.Log("Transfer!");
+        Debug.Log("Transfer1!");
 
         SpriteRenderer destinationRenderer = destinationCard.GetComponent<SpriteRenderer>();
 
-        if (pickUpScript.deck.Count > 0 && destinationRenderer != null)
+        if (pickUpScript != null && pickUpScript.deck.Count > 0 && destinationRenderer != null)
         {
-            Debug.Log("Transfer!");
+            Debug.Log("Transfer2!");
 
             int randomIndex = Random.Range(0, pickUpScript.deck.Count);
             Card sourceCard = pickUpScript.deck[randomIndex];
 
-            destinationRenderer.sprite = sourceCard.GetComponent<SpriteRenderer>().sprite;
-            destinationRenderer.enabled = true; 
-            destinationCard.transform.position = cardSlots[randomIndex].position;
+            SpriteRenderer sourceRenderer = sourceCard.GetComponent<SpriteRenderer>();
 
-            pickUpScript.deck.RemoveAt(randomIndex);
-            sourceCard.gameObject.SetActive(false);
+            if (sourceRenderer != null)
+            {
+                destinationRenderer.sprite = sourceRenderer.sprite;
+                destinationRenderer.enabled = true; // Enable the sprite renderer in the destination
+                destinationCard.transform.position = cardSlots[randomIndex].position;
+
+                pickUpScript.deck.RemoveAt(randomIndex);
+                sourceCard.gameObject.SetActive(false);
+            }
+            else
+            {
+                Debug.Log("Source SpriteRenderer not found!");
+            }
         }
     }
 }
